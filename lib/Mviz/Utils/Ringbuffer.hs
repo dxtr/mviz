de35683692fe2eb156175816256652f20e3ffc3c
dxtr <dxtr>
2023-08-10 22:02:16 +0000

    Update the ringbuffer

--- lib/Mviz/Utils/Ringbuffer.hs
+++ lib/Mviz/Utils/Ringbuffer.hs
@@ -1,52 +1,57 @@
 module Mviz.Utils.Ringbuffer (Ringbuffer, make, empty, put, next) where
 
 import Control.Concurrent.STM (atomically)
-import Control.Concurrent.STM.TVar
+import Control.Concurrent.STM.TVar (
+  TVar,
+  newTVarIO,
+  readTVar,
+  stateTVar,
+  writeTVar,
+ )
+import Data.Foldable (foldl')
 import Data.Sequence qualified as S
 
 data Ringbuffer a = Ringbuffer
-  { ringWriterIndex :: TVar Int
-  , ringReaderIndex :: TVar Int
-  , ringSize :: Int -- This is static so doesn't have to be a TVar
+  { ringWriterIndex :: TVar Word
+  , ringReaderIndex :: TVar Word
+  , ringSize :: Word -- This is static so doesn't have to be a TVar
   , ringBuffer :: S.Seq (TVar (Maybe a)) -- Only the items are mutable, not the buffer itself
   }
 
-nextIndex :: Ringbuffer a -> Int -> Int
+nextIndex :: Ringbuffer a -> Word -> Word
 nextIndex Ringbuffer{ringSize = size} idx = (idx + 1) `mod` size
 
-make :: [a] -> IO (Ringbuffer a)
-make items = do
+make_ :: [Maybe a] -> IO (Ringbuffer a)
+make_ items = do
   wIdx <- newTVarIO 0
   rIdx <- newTVarIO 0
-  buf <- mapM (newTVarIO . Just) items
+  buf <- mapM newTVarIO items
   return $
     Ringbuffer
       { ringWriterIndex = wIdx
       , ringReaderIndex = rIdx
-      , ringSize = length items
+      , ringSize = foldl' (\c _ -> c + 1) 0 items
       , ringBuffer = S.fromList buf
       }
+
+make :: [a] -> IO (Ringbuffer a)
+make items = make_ $ map Just items
 
 empty :: Int -> IO (Ringbuffer a)
-empty size = do
-  wIdx <- newTVarIO 0
-  rIdx <- newTVarIO 0
-  buf <- mapM newTVarIO $ replicate size Nothing
-  return $
-    Ringbuffer
-      { ringWriterIndex = wIdx
-      , ringReaderIndex = rIdx
-      , ringSize = size
-      , ringBuffer = S.fromList buf
-      }
+empty size = make_ $ replicate size Nothing
 
 put :: Ringbuffer a -> a -> IO ()
-put ringBuffer@Ringbuffer{ringBuffer = buffer, ringWriterIndex = wIndex} newItem = atomically $ do
-  wIndex_ <- stateTVar wIndex (\idx -> (idx, nextIndex ringBuffer idx))
-  let bufItem = S.index buffer wIndex_
-  writeTVar bufItem $ Just newItem
+put ringBuffer@Ringbuffer{ringBuffer = buffer, ringWriterIndex = wIndex} newItem =
+  atomically $
+    S.index buffer <$> stateTVar wIndex updateIndexFunc
+      >>= ((flip writeTVar) $ Just newItem)
+ where
+  updateIndexFunc idx = (fromIntegral idx, nextIndex ringBuffer idx)
 
 next :: Ringbuffer a -> IO (Maybe a)
-next ringBuffer@Ringbuffer{ringBuffer = buffer, ringReaderIndex = rIndex} = atomically $ do
-  rIndex_ <- stateTVar rIndex (\idx -> (idx, nextIndex ringBuffer idx))
-  readTVar $ S.index buffer rIndex_
+next ringBuffer@Ringbuffer{ringBuffer = buffer, ringReaderIndex = rIndex} =
+  atomically $
+    readTVar
+      =<< S.index buffer <$> stateTVar rIndex updateIndexFunc
+ where
+  updateIndexFunc idx = (fromIntegral idx, nextIndex ringBuffer idx)
